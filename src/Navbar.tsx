540cc44f5ed49bee75315223a1f9b9f621d80556
Chazz Carrizales <czcarrizales@yahoo.com>
2023-10-09 19:23:22 -0700

    Links for Navbar component

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+        <Link to="/">Logo</Link>
+        <Link to="/">Overview</Link>
+        <Link to="/analytics">Analytics</Link>
+    </div>
   )
 }
 
