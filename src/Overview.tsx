1f76be647901bd13be9d578888ac9abcef302080
Chazz Carrizales <czcarrizales@yahoo.com>
2023-10-11 14:23:54 -0700

    using redux toolkit and circle-progressbar

--- src/Overview.tsx
+++ src/Overview.tsx
@@ -1,41 +1,58 @@
-import React from 'react'
+import FoodLog from './FoodLog'
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css';
 import './Overview.css'
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const Overview = () => {
-  return (
-    <div className='overview-main-container'>
-    <div className='app-calorie-container'>
-        <h1>Calories</h1>
-        <h2>1000</h2>
-      </div>
-      <div className='app-overview-details-container'>
-        <div className='app-overview-detail'>
-          <h3>Fats</h3>
-          <p>Amount</p>
+
+    const user = useSelector((state) => state.user)
+
+    const styles = buildStyles({
+        strokeLinecap: 'round',
+        pathTransitionDuration: 0.5,
+        textSize: '16px',
+        pathColor: '#2BBC66'
+    });
+
+    return (
+        <div className='overview-main-container'>
+            <CircularProgressbarWithChildren className='app-calorie-container' value={(user.currentCalories / user.maxCalories) * 100} strokeWidth={4} styles={styles}>
+                    <h1>Calories</h1>
+                    <h2>{user.currentCalories}</h2>
+
+            </CircularProgressbarWithChildren>
+            <div className='app-overview-details-container'>
+                <div className='app-overview-detail'>
+                    <h3>Weight</h3>
+                    <p>{user.currentWeight} lb</p>
+                </div>
+                <div className='app-overview-detail'>
+                    <h3>Protein</h3>
+                    <p>{user.currentProtein}g</p>
+                </div>
+                <div className='app-overview-detail'>
+                    <h3>Fats</h3>
+                    <p>{user.currentFat}g</p>
+                </div>
+                <div className='app-overview-detail'>
+                    <h3>Carbs</h3>
+                    <p>{user.currentCarbs}g</p>
+                </div>
+            </div>
+            <div className='overview-food-log-container'>
+                <h3>Food Log</h3>
+                {
+                    user.foodLog.map((food) => {
+                        return (
+                            <FoodLog name={food.name} calories={food.calories} protein={food.protein} fat={food.fat} carbs={food.carbs} />
+                        )
+                    })
+                }
+            </div>
         </div>
-        <div className='app-overview-detail'>
-          <h3>Fats</h3>
-          <p>Amount</p>
-        </div>
-        <div className='app-overview-detail'>
-          <h3>Fats</h3>
-          <p>Amount</p>
-        </div>
-        <div className='app-overview-detail'>
-          <h3>Fats</h3>
-          <p>Amount</p>
-        </div>
-      </div>
-      <div>
-        <div className='app-log-container'>
-          <p className='app-log-picture'>Picture</p>
-          <p>Name</p>
-          <p>Calories</p>
-          <p>Date</p>
-        </div>
-      </div>
-      </div>
-  )
+    )
 }
 
 export default Overview
