97f8145edf636bcc1e0d91e3c3080fe7b99733e0
Chazz Carrizales <czcarrizales@yahoo.com>
2023-10-12 11:36:24 -0700

    style modal for adding food

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './Modal.css'
-import { useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { addToFoodLog, updateCurrentCalories, updateCurrentCarbs, updateCurrentFat, updateCurrentProtein } from './slices/userSlice'
 
 interface ModalProps {
     setShowModal: any;
 }
 
-const Modal: React.FC<ModalProps> = ({setShowModal}) => {
+const Modal: React.FC<ModalProps> = ({ setShowModal }) => {
 
     const dispatch = useDispatch()
     const [foodName, setFoodName] = useState('')
@@ -35,35 +35,46 @@ const Modal: React.FC<ModalProps> = ({setShowModal}) => {
         }))
     }
 
-  return (
-    <div className='modal-container'>
-        <div className="modal-content">
-            <h2>Add Food</h2>
-            <label htmlFor="name">
-                Food Name:
-                <input type="text" onChange={(e) => setFoodName(e.target.value)}  />
-            </label>
-            <label htmlFor="calories">
-                Calories:
-                <input type="number" onChange={(e) => setFoodCalories(parseInt(e.target.value))} />
-            </label>
-            <label htmlFor="protein">
-                Protein:
-                <input type="number" onChange={(e) => setFoodProtein(parseInt(e.target.value))} />
-            </label>
-            <label htmlFor="fats">
-                Fats:
-                <input type="number" onChange={(e) => setFoodFat(parseInt(e.target.value))} />
-            </label>
-            <label htmlFor="carbs">
-                Carbs:
-                <input type="number" onChange={(e) => setFoodCarbs(parseInt(e.target.value))} />
-            </label>
-            <button onClick={handleCancelButton}>Cancel</button>
-            <button onClick={handleAddButton}>Add</button>
+    return (
+        <div className='modal-container'>
+            <div className="modal-content">
+                <h2>Add Food</h2>
+                <label htmlFor="name">
+                    Name
+                </label>
+                <input className='nutrition-info-input' type="text" onChange={(e) => setFoodName(e.target.value)} required />
+                <div className='nutrition-info-container'>
+                    <div className='nutrition-info-details'>
+                        <label htmlFor="calories">
+                            Calories
+                        </label>
+                        <input className='nutrition-info-input' type="number" onChange={(e) => setFoodCalories(parseInt(e.target.value))} required />
+                    </div>
+                    <div className="nutrition-info-details">
+                        <label htmlFor="protein">
+                            Protein
+                        </label>
+                        <input className='nutrition-info-input' type="number" onChange={(e) => setFoodProtein(parseInt(e.target.value))} />
+                    </div>
+                    <div className="nutrition-info-details">
+                        <label htmlFor="fats">
+                            Fats
+                        </label>
+                        <input className='nutrition-info-input' type="number" onChange={(e) => setFoodFat(parseInt(e.target.value))} />
+                    </div>
+                    <div className="nutrition-info-details">
+                        <label htmlFor="carbs">
+                            Carbs
+                        </label>
+                        <input className='nutrition-info-input' type="number" onChange={(e) => setFoodCarbs(parseInt(e.target.value))} />
+                    </div>
+                </div>
+                <button className='add-food-button' onClick={handleAddButton}>Add</button>
+                <button className='cancel-food-button' onClick={handleCancelButton}>Cancel</button>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Modal
